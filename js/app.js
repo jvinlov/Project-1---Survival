3f6e77de59df1cc99db8ae4345ae1b70043510aa
Josh Vinlove <jjvinlove@gmail.com>
2019-10-07 15:46:35 -0600

    added wait time

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
+function wait(ms) {
+		var d = new Date();
+		var d2 = null;
+		do { d2 = new Date(); }
+		while(d2-d < ms);
+	}
+
 $('button').on('click', (e) => {
 	e.preventDefault();
 	const $villageName = $('#villageInput').val();
-	console.log($villageName);
+	console.log(`${$villageName}!`);
 	$('#villageInput').remove();
 	$('#submit-btn').remove();
-	$('.inputs').append($villageName);
+	$('.inputs').append(`${$villageName}!`);
 	$('#location h2').text('Long live');
+	
+	wait (2000);
 	$('.environment h2').text('Click your Environment');
+	
+	$('.pics_in_a_row').css('display', 'flex');
 
 });
 
@@ -15,8 +26,11 @@ $('.pics_in_a_row').on('click', (e) =>{
 	const $playerEnvironment = $(e.target).attr('id');
 	console.log($playerEnvironment);
 	$('.environment h2').text(`You picked ${$playerEnvironment}`);
+	$('.disasterPics').css('display', 'flex');
+	wait (3000);
 
 });
+
 
 
 
