b748ce35d0f125f4ff909879a7376248acdc90aa
Josh Vinlove <jjvinlove@gmail.com>
2019-10-07 15:16:21 -0600

    first inputs assigned

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-const villageName = 
+$('button').on('click', (e) => {
+	e.preventDefault();
+	const $villageName = $('#villageInput').val();
+	console.log($villageName);
+	$('#villageInput').remove();
+	$('#submit-btn').remove();
+	$('.inputs').append($villageName);
+	$('#location h2').text('Long live');
+	$('.environment h2').text('Click your Environment');
+
+});
+
+$('.pics_in_a_row').on('click', (e) =>{
+	e.preventDefault();
+	const $playerEnvironment = $(e.target).attr('id');
+	console.log($playerEnvironment);
+	$('.environment h2').text(`You picked ${$playerEnvironment}`);
+
+});
+
+
+
+
+
+
+
+
 
 
