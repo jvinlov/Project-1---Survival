b01483691279be6f72d1c67ba085b7ffa2367cbf
Josh Vinlove <jjvinlove@gmail.com>
2019-10-08 10:18:09 -0600

    added stats block and classes

--- js/app.js
+++ js/app.js
@@ -3,18 +3,24 @@ function wait(ms) {
 		var d2 = null;
 		do { d2 = new Date(); }
 		while(d2-d < ms);
-	}
+	};
+
+let $continu = $('<button type="submit" id="continue-btn">Continue...</button>')
+
+let villageName;
+
+let playerPicture;
 
 $('button').on('click', (e) => {
 	e.preventDefault();
-	const $villageName = $('#villageInput').val();
-	console.log(`${$villageName}!`);
+	villageName = $('#villageInput').val();
+	
 	$('#villageInput').remove();
 	$('#submit-btn').remove();
-	$('.inputs').append(`${$villageName}!`);
+	$('.inputs').append(`${villageName}!`);
 	$('#location h2').text('Long live');
 	
-	wait (2000);
+	wait (100);
 	$('.environment h2').text('Click your Environment');
 	
 	$('.pics_in_a_row').css('display', 'flex');
@@ -24,12 +30,64 @@ $('button').on('click', (e) => {
 $('.pics_in_a_row').on('click', (e) =>{
 	e.preventDefault();
 	const $playerEnvironment = $(e.target).attr('id');
-	console.log($playerEnvironment);
+	const $playerPicture = $(e.target).attr('src');
+	
 	$('.environment h2').text(`You picked ${$playerEnvironment}`);
 	$('.disasterPics').css('display', 'flex');
-	wait (3000);
+	$continu.on('click', (e) => {
+		e.preventDefault();
+		console.log('yes');
+		$('.grid-container1').css('display', 'none');
+		$('.grid-container2').css('display', 'grid');
+		$('#playerVillage h3').text(villageName);
+		$('#playerVillage').html(playerPicture);
+	});
+	$('#disasters h2').append($continu)
+	wait (500);
+
 
 });
+
+
+
+
+class Village {
+
+	constructor() {
+		this.population = 20;
+		this.hut=2;
+		this.hiRise=0;
+		this.credits=200;
+		this.score= 40;
+	}
+};
+
+const civ = new Village;
+console.log(civ);
+
+class Building {
+
+	constructor (score, population, cost, buildTime) {
+
+	this.score = score;
+	this.population = population;
+	this.cost = cost;
+	this.buildTime = buildTime;
+
+	}
+};
+
+const hiRise = new Building(30, 20, 100, 3000);
+console.log(hiRise);
+
+const hut = new Building(10, 5, 20, 1000);
+console.log(hut);
+
+
+
+
+
+
 
 
 
